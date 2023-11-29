f084c2834ff18b24b677365cf2cf2a31be7ef3ee
colacadstink <benharris5@gmail.com>
2023-11-29 10:43:03 -0500

    Fixed super rare rarity

--- src/types/Rarity.ts
+++ src/types/Rarity.ts
@@ -2,7 +2,7 @@ export enum Rarity {
   Common = "Common",
   Uncommon = "Uncommon",
   Rare = "Rare",
-  SuperRare = "Super-Rare",
+  SuperRare = "Super Rare",
   Legendary = "Legendary",
 }
 export const Rarities = Object.values(Rarity);
